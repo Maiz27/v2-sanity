7170955900889fdbc43aa6b37a72cf263414e9b6
Maged Faiz <magedfaiz98@gmail.com>
2024-04-09 00:18:31 +0200

    feat: Add project and tool schema types

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -1,13 +1,13 @@
 import about from './about'
 import experience from './experience'
+import project from './project'
+import tool from './tool'
 
 import {stats} from './objects/stats'
 import {duration} from './objects/duration'
 import {company} from './objects/company'
-import jobDescription from './objects/jobDescription'
-import project from './project'
+import {jobDescription} from './objects/jobDescription'
 import {projectImage} from './objects/projectImage'
-import tool from './tool'
 
 export const schemaTypes = [
   about,
--- schemaTypes/objects/jobDescription.ts
+++ schemaTypes/objects/jobDescription.ts
@@ -1,6 +1,6 @@
 import {defineType, defineArrayMember} from 'sanity'
 
-export default defineType({
+export const jobDescription = defineType({
   title: 'Job Description',
   name: 'description',
   type: 'array',
